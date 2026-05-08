a1f944c4a88d4c37df0c8a2d80f8511fe1a10330
kayceekool <115635448+kayceekool@users.noreply.github.com>
2026-05-09 00:23:03 +0100

    Update login.js

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -1,1 +1,18 @@
-export default function Login(){return <h1>Login</h1>}
+export default function Login() {
+
+  return (
+
+    <div>
+
+      <h1>Login</h1>
+
+      <input placeholder="Email" />
+      <input placeholder="Password" />
+
+      <button>
+        Login
+      </button>
+
+    </div>
+  );
+}
