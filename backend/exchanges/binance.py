8bc4c4e8939ce642305ab3eec88d65fb52609110
kayceekool <115635448+kayceekool@users.noreply.github.com>
2026-05-09 00:17:07 +0100

    Create exchanges/binance.py

---
+++ backend/exchanges/binance.py
@@ -0,0 +1,8 @@
+def execute_binance_trade(symbol, amount):
+
+    return {
+        "exchange": "binance",
+        "symbol": symbol,
+        "amount": amount,
+        "status": "paper_mode"
+    }
