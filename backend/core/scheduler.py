6416fae70b34cabd964ca1a75dc5f94b652ab254
kayceekool <115635448+kayceekool@users.noreply.github.com>
2026-06-02 02:38:53 +0100

    Create backend/core/scheduler.py

---
+++ backend/core/scheduler.py
@@ -0,0 +1,29 @@
+import asyncio
+
+from scanners.launch_scanner import launch_loop
+from scanners.dex_loop import dex_loop
+from scanners.migration_scanner import migration_loop
+from scanners.wallet_scanner import wallet_loop
+
+
+async def start_scheduler():
+
+    asyncio.create_task(
+        dex_loop()
+    )
+
+    asyncio.create_task(
+        launch_loop()
+    )
+
+    asyncio.create_task(
+        migration_loop()
+    )
+
+    asyncio.create_task(
+        wallet_loop()
+    )
+
+    print(
+        "All scanner tasks started"
+    )
