f06ca1015331376ef2f0ad8550c998f8d560d28c
kayceekool <115635448+kayceekool@users.noreply.github.com>
2026-06-06 00:54:37 +0100

    Create backend/tasks/live_feed_loop.py

---
+++ backend/tasks/live_feed_loop.py
@@ -0,0 +1,23 @@
+import asyncio
+
+from services.live_feed import (
+    LiveFeed
+)
+
+
+async def live_feed_loop():
+
+    while True:
+
+        try:
+
+            await LiveFeed.update()
+
+        except Exception as e:
+
+            print(
+                "LIVE FEED ERROR",
+                e
+            )
+
+        await asyncio.sleep(15)
