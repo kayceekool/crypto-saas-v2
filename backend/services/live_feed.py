10b4fce815ba4fc610f4f6d0ff2586752ae5f01d
kayceekool <115635448+kayceekool@users.noreply.github.com>
2026-06-06 00:53:47 +0100

    Create backend/services/live_feed.py

---
+++ backend/services/live_feed.py
@@ -0,0 +1,27 @@
+from scanners.dex_scanner import (
+    DexScanner
+)
+
+from services.intelligence_hub import (
+    IntelligenceHub
+)
+
+
+class LiveFeed:
+
+    scanner = DexScanner()
+
+    @classmethod
+    async def update(cls):
+
+        tokens = await cls.scanner.search(
+            "sol"
+        )
+
+        ranked = await (
+            IntelligenceHub.process(
+                tokens
+            )
+        )
+
+        return ranked
