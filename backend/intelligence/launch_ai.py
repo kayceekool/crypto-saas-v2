e4be9f1f37d8365037399d429556a228fc908d67
kayceekool <115635448+kayceekool@users.noreply.github.com>
2026-06-06 20:17:51 +0100

    Update launch_ai.py

--- backend/intelligence/launch_ai.py
+++ backend/intelligence/launch_ai.py
@@ -10,19 +10,29 @@ class LaunchAI:
             999
         )
 
-        if age < 1:
-            score += 150
-
-        elif age < 3:
-            score += 100
-
-        elif age < 6:
-            score += 50
-
-        if token.get(
+        liquidity = token.get(
             "liquidity",
             0
-        ) > 25000:
+        )
+
+        volume = token.get(
+            "volume",
+            0
+        )
+
+        if age < 1:
+            score += 300
+
+        elif age < 3:
+            score += 200
+
+        elif age < 6:
+            score += 100
+
+        if liquidity > 25000:
             score += 50
+
+        if volume > liquidity:
+            score += 100
 
         return score
