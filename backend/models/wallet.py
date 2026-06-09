9e91919dc792388b085796d8e200ba224fc6155a
kayceekool <115635448+kayceekool@users.noreply.github.com>
2026-06-09 22:46:19 +0100

    Update wallet.py

--- backend/models/wallet.py
+++ backend/models/wallet.py
@@ -10,20 +10,66 @@ class Wallet(Base):
 
     __tablename__ = "wallets"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(
+        Integer,
+        primary_key=True
+    )
 
-    wallet = Column(String, unique=True)
+    wallet = Column(
+        String,
+        unique=True
+    )
 
-    total_trades = Column(Integer, default=0)
+    total_trades = Column(
+        Integer,
+        default=0
+    )
 
-    winning_trades = Column(Integer, default=0)
+    winning_trades = Column(
+        Integer,
+        default=0
+    )
 
-    losing_trades = Column(Integer, default=0)
+    losing_trades = Column(
+        Integer,
+        default=0
+    )
 
-    win_rate = Column(Float, default=0)
+    win_rate = Column(
+        Float,
+        default=0
+    )
 
-    roi = Column(Float, default=0)
+    roi = Column(
+        Float,
+        default=0
+    )
 
-    score = Column(Float, default=0)
+    score = Column(
+        Float,
+        default=0
+    )
 
-    rank = Column(String, default="UNRANKED")
+    rank = Column(
+        String,
+        default="UNRANKED"
+    )
+
+    last_seen = Column(
+        String
+    )
+
+    followed = Column(
+        Integer,
+        default=0
+    )
+
+    copytrade_enabled = Column(
+        Integer,
+        default=0
+    )
+
+    smart_wallet = Column(
+        Integer,
+        default=0
+    )
