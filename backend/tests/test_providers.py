b6780c7bdb8dd6f3a9dfe6b18244a3f3e4b2f941
kayceekool <115635448+kayceekool@users.noreply.github.com>
2026-08-12 13:57:19 +0100

    Create backend/tests/test_providers.py

---
+++ backend/tests/test_providers.py
@@ -0,0 +1,11 @@
+from backend.providers.base import (
+    BaseProvider,
+)
+
+from backend.providers.models import (
+    TokenMarketData,
+)
+
+from backend.providers.provider import (
+    ProviderRegistry,
+)
